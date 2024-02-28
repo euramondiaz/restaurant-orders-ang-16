f044855462f2aa41f856801e4077a7f880ea738b
Ramon Dias <ramondiaz.ads@gmail.com>
2024-02-28 14:37:12 -0300

    correcting class

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -21,35 +21,35 @@ export class OrderDetailsService {
       id:2,
       foodName:"Veggie Supreme",
       foodDetails:"Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
-      foodPrice:369,
+      foodPrice:30,
       foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
     },
     {
       id:3,
       foodName:"Paneer Burger",
       foodDetails:"panner",
-      foodPrice:149,
+      foodPrice:20,
       foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
     },
     {
       id:4,
       foodName:"Veg Masala Roll In Naan",
       foodDetails:"A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
-      foodPrice:140,
+      foodPrice:15,
       foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
     },
     {
       id:5,
       foodName:"Indulgence Brownie",
       foodDetails:"(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.",
-      foodPrice:105,
+      foodPrice:10,
       foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv"
     },
     {
       id:6,
-      foodName:"Oreo Cheesecake Ice Cream",
+      foodName:"Oreo Cheesecake Ice Cream 300g",
       foodDetails:"Oreo ice cream",
-      foodPrice:219,
+      foodPrice:22,
       foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6"
     }
   ]
